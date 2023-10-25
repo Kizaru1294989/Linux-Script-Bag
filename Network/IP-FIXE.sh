0bd66cf6888d41f915f06aef5c0d6c4339249ada
Ryan RAIS <rrais@exaprobe.com>
2023-10-25 15:47:52 +0200

    p

--- Network/IP-FIXE.sh
+++ Network/IP-FIXE.sh
@@ -3,13 +3,16 @@
 # Récupérer le nom de la carte réseau
 network_interface=$(ip route | grep default | awk '{print $5}')
 
-# Récupérer l'adresse IP
-ip_address=$(ip -4 -o addr show dev "$network_interface" | awk '{print $4}' | cut -d'/' -f1)
+# Récupérer l'adresse IP et le masque en /24
+ip_info=$(ip -4 -o addr show dev "$network_interface" | awk '$3 == "inet" {print $4}')
 
 # Récupérer la passerelle (gateway)
 gateway=$(ip route | grep default | awk '{print $3}')
 
+# Concaténer l'adresse IP et le masque
+ip_with_mask="$ip_info"
+
 # Afficher les informations
 echo "Carte réseau : $network_interface"
-echo "Adresse IP : $ip_address"
+echo "Adresse IP avec masque : $ip_with_mask"
 echo "Passerelle (Gateway) : $gateway"
