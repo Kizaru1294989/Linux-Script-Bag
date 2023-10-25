a57088443394f09fe0d7d067aa2e6ad934fe2064
Ryan RAIS <rrais@exaprobe.com>
2023-10-25 15:46:26 +0200

    fix

--- Network/IP-FIXE.sh
+++ Network/IP-FIXE.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Récupérer l'adresse IP de la carte réseau "ens192"
-interface="ens192"
-ip_info=$(ip a | grep "$interface:" -A2 | sed -n 's/.*inet \([0-9.]\+\)\/[0-9]\+ .*/\1/p')
+# Récupérer le nom de la carte réseau
+network_interface=$(ip route | grep default | awk '{print $5}')
 
-# Récupérer l'adresse IP de la passerelle (gateway)
+# Récupérer l'adresse IP
+ip_address=$(ip -4 -o addr show dev "$network_interface" | awk '{print $4}' | cut -d'/' -f1)
+
+# Récupérer la passerelle (gateway)
 gateway=$(ip route | grep default | awk '{print $3}')
 
-# Afficher les résultats
-echo "Carte réseau : $interface"
-echo "Adresse IP : $ip_info"
+# Afficher les informations
+echo "Carte réseau : $network_interface"
+echo "Adresse IP : $ip_address"
 echo "Passerelle (Gateway) : $gateway"
